35c9f712a6e4248df1929f702d8a8d94ef066bea
Chris Lalancette <clalancette@openrobotics.org>
2017-08-18 11:09:43 -0400

    Add a Compound type to the zero-initialization-helper-demo.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- zero-initialization-helper-demo.cc
+++ zero-initialization-helper-demo.cc
@@ -47,6 +47,52 @@ struct Float64_
 using Float64 =
     std_msgs::msg::Float64_<std::allocator<void>>;
 
+// message struct
+template<class ContainerAllocator>
+struct Compound_
+{
+  using Type = Compound_<ContainerAllocator>;
+
+  Compound_()
+  {
+  }
+  explicit Compound_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+
+  using _float64_type =
+      std_msgs::msg::Float64_<ContainerAllocator>;
+  using _data_type =
+      double;
+
+  _float64_type float64;
+  _data_type data;
+
+  Type * set__float64(
+    const std_msgs::msg::Float64_<ContainerAllocator> & _arg)
+  {
+    this->float64 = _arg;
+    return this;
+  }
+
+  Type * set__data(
+    const double & _arg)
+  {
+    this->data = _arg;
+    return this;
+  }
+
+  void zero_initialize()
+  {
+    this->float64.zero_initialize();
+    this->data = 0.0;
+  }
+};
+
+using Compound =
+    std_msgs::msg::Compound_<std::allocator<void>>;
+
 }
 }
 
@@ -63,6 +109,17 @@ int main(void)
   std_msgs::msg::Float64 init;
   init.zero_initialize();
   std::cout << init.data << std::endl;
+
+  // For compound types (such as Compound), the same problem applies
+  //std::msgs::msg::Compound c_uninit;
+  //std::cout << c_uninit.data << std::endl;
+  //std::cout << c_uninit.data << std::endl;
+
+  // Having a helper to zero initialize all of the fields helps a lot.  The user still needs to remember to call this.
+  std_msgs::msg::Compound c_init;
+  c_init.zero_initialize();
+  std::cout << c_init.float64.data << std::endl;
+  std::cout << c_init.data << std::endl;
 
   return 0;
 }
