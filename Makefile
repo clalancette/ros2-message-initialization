f094de2f9e48793366e5674e3e7989990f1919e1
Chris Lalancette <clalancette@openrobotics.org>
2017-08-18 15:51:14 -0400

    Add in an inheritance-demo.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX=g++
 CXXFLAGS=-g -Wall -Wextra -std=c++14
 
-all: no-initialization-demo zero-initialization-helper-demo factory-helper-demo constructor-init-demo
+all: no-initialization-demo zero-initialization-helper-demo factory-helper-demo constructor-init-demo inheritance-demo
 
 no-initialization-demo: no-initialization-demo.cc
 	$(CXX) $(CXXFLAGS) -o $@ $<
@@ -15,5 +15,8 @@ factory-helper-demo: factory-helper-demo.cc
 constructor-init-demo: constructor-init-demo.cc
 	$(CXX) $(CXXFLAGS) -o $@ $<
 
+inheritance-demo: inheritance-demo.cc
+	$(CXX) $(CXXFLAGS) -o $@ $<
+
 clean:
-	rm -f *~ *.o no-initialization-demo zero-initialization-helper-demo factory-helper-demo constructor-init-demo
+	rm -f *~ *.o no-initialization-demo zero-initialization-helper-demo factory-helper-demo constructor-init-demo inheritance-demo
--- inheritance-demo.cc
+++ inheritance-demo.cc
@@ -39,10 +39,50 @@ struct Float64_
     return this;
   }
 
+  void zero_initialize()
+  {
+    this->data = 0.0;
+  }
 };
 
 using Float64 =
     std_msgs::msg::Float64_<std::allocator<void>>;
+
+template<class ContainerAllocator>
+struct Float64_init_ : public Float64_<ContainerAllocator>
+{
+  using Type = Float64_init_<ContainerAllocator>;
+
+  Float64_init_()
+  {
+    this->zero_initialize();
+  }
+  explicit Float64_init_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+};
+
+using Float64_init =
+    std_msgs::msg::Float64_init_<std::allocator<void>>;
+
+template<class ContainerAllocator>
+struct Float64_uninit_ : public Float64_<ContainerAllocator>
+{
+  using Type = Float64_uninit_<ContainerAllocator>;
+
+  Float64_uninit_()
+  {
+  }
+  explicit Float64_uninit_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+};
+
+using Float64_uninit =
+    std_msgs::msg::Float64_uninit_<std::allocator<void>>;
+
 
 // This is a fake "Compound" class that I made up, that embeds a Float64 and
 // primitive fields within it.  It doesn't exist in ROS2, but the below is what
@@ -85,10 +125,50 @@ struct Compound_
     return this;
   }
 
+  void zero_initialize()
+  {
+    this->float64.zero_initialize();
+    this->data = 0.0;
+  }
 };
 
 using Compound =
     std_msgs::msg::Compound_<std::allocator<void>>;
+
+template<class ContainerAllocator>
+struct Compound_init_ : public Compound_<ContainerAllocator>
+{
+  using Type = Compound_init_<ContainerAllocator>;
+
+  Compound_init_()
+  {
+    this->zero_initialize();
+  }
+  explicit Compound_init_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+};
+
+using Compound_init =
+    std_msgs::msg::Compound_init_<std::allocator<void>>;
+
+template<class ContainerAllocator>
+struct Compound_uninit_ : public Compound_<ContainerAllocator>
+{
+  using Type = Compound_uninit_<ContainerAllocator>;
+
+  Compound_uninit_()
+  {
+  }
+  explicit Compound_uninit_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+};
+
+using Compound_uninit =
+    std_msgs::msg::Compound_uninit_<std::allocator<void>>;
 
 }
 }
@@ -99,25 +179,20 @@ int main(void)
 
   // If you uncomment this and run this under valgrind, you'll see valgrind
   // complain about accessing an uninitialized variable, which makes sense.
-  //std_msgs::msg::Float64 uninit;
+  //std_msgs::msg::Float64_uninit uninit;
   //std::cout << uninit.data << std::endl;
 
   // Instead, if you manually initialize the fields, then valgrind is happy.
-  std_msgs::msg::Float64 init;
-  init.set__data(0.0);
+  std_msgs::msg::Float64_init init;
   std::cout << init.data << std::endl;
 
   // For compound types (such as Compound), the same problem applies
-  //std_msgs::msg::Compound c_uninit;
-  //std::cout << c_uninit.data << std::endl;
-  //std::cout << c_uninit.data << std::endl;
+  // std_msgs::msg::Compound_uninit c_uninit;
+  // std::cout << c_uninit.data << std::endl;
+  // std::cout << c_uninit.data << std::endl;
 
   // Manually initializing all of the fields for Compound types gets more tedious and error-prone.
-  std_msgs::msg::Compound c_init;
-  c_init.set__data(0.0);
-  std_msgs::msg::Float64 f64;
-  f64.set__data(0.0);
-  c_init.set__float64(f64);
+  std_msgs::msg::Compound_init c_init;
   std::cout << c_init.float64.data << std::endl;
   std::cout << c_init.data << std::endl;
 
