7a82900bdefe365531f47d1f5846738683704ba4
Chris Lalancette <clalancette@openrobotics.org>
2017-08-18 11:16:11 -0400

    Add a Compound example to constructor-init-demo.cc
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- constructor-init-demo.cc
+++ constructor-init-demo.cc
@@ -49,6 +49,55 @@ struct Float64_
 
 using Float64 =
     std_msgs::msg::Float64_<std::allocator<void>>;
+
+// message struct
+template<class ContainerAllocator>
+struct Compound_
+{
+  using Type = Compound_<ContainerAllocator>;
+
+  Compound_(bool default_initialize=false)
+  {
+    if (default_initialize) {
+      this->zero_initialize();
+    }
+  }
+  explicit Compound_(const ContainerAllocator & _alloc)
+  {
+    (void)_alloc;
+  }
+
+  using _float64_type =
+      std_msgs::msg::Float64_<ContainerAllocator>;
+  using _data_type =
+      double;
+
+  _float64_type float64;
+  _data_type data;
+
+  Type * set__float64(
+    const std_msgs::msg::Float64_<ContainerAllocator> & _arg)
+  {
+    this->float64 = _arg;
+    return this;
+  }
+
+  Type * set__data(
+    const double & _arg)
+  {
+    this->data = _arg;
+    return this;
+  }
+
+  void zero_initialize()
+  {
+    this->float64.zero_initialize();
+    this->data = 0.0;
+  }
+};
+
+using Compound =
+    std_msgs::msg::Compound_<std::allocator<void>>;
 }
 }
 
@@ -65,6 +114,16 @@ int main(void)
   // Instead, if you tell the constructor to initialize the fields, then valgrind is happy.
   std_msgs::msg::Float64 init(true);
   std::cout << init.data << std::endl;
+
+  // For compound types (such as Compound), the same problem applies
+  // std_msgs::msg::Compound c_uninit(false);
+  // std::cout << c_uninit.float64.data << std::endl;
+  // std::cout << c_uninit.data << std::endl;
+
+  // Having a helper to zero initialize all of the fields helps a lot.  The user still needs to remember to call this.
+  std_msgs::msg::Compound c_init(true);
+  std::cout << c_init.float64.data << std::endl;
+  std::cout << c_init.data << std::endl;
 
   return 0;
 }
